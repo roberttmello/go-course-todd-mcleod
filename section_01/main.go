4b23094a5254c6021952a347326a8f367e866630
Carlos Robert <crobertdmelo@gmail.com>
2024-01-11 17:19:05 -0300

    update section_01/main.go

--- section_01/main.go
+++ section_01/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hello Gophers!");
+func main() {
+	const name, age = "Ted", 28
+	fmt.Println("Hello Gophers!")
+	fmt.Printf("\n")
+	fmt.Printf("1----- %s is %d years old.\n", name, age)
+	fmt.Printf("2----- %v is %v years old.\n", name, age)
+	fmt.Printf("3----- %s is %d years old. \t The type of the variables is respectively: %T and %T\n", name, age, name, age)
 }
